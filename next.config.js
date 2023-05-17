7a3cd78ed35102dfadd1eaccfad57b4bbf1e2aab
DaveyReno <38965819+daveyreno@users.noreply.github.com>
2023-05-17 15:30:27 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['ouleipcuazzmcbojpbet.supabase.co'], // Replace 'example.com' with the domain that hosts your images
+    },
+};
+
+module.exports = nextConfig;
